97fd6fa91112f291c12cff78d3cecf7e714fbb2c
drone-spec <drone@dronemothership.work>
2024-01-31 20:39:20 -0600

    Hey-o That is hello world!

---
+++ target/debug/deps/busty_rusty-2ac7b527293ab3a6.d
@@ -0,0 +1,5 @@
+/home/drone/git/busty_rust/target/debug/deps/libbusty_rusty-2ac7b527293ab3a6.rmeta: src/main.rs
+
+/home/drone/git/busty_rust/target/debug/deps/busty_rusty-2ac7b527293ab3a6.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/busty_rusty-d7b2df6a3a1f132e.d
@@ -0,0 +1,5 @@
+/home/drone/git/busty_rust/target/debug/deps/libbusty_rusty-d7b2df6a3a1f132e.rmeta: src/main.rs
+
+/home/drone/git/busty_rust/target/debug/deps/busty_rusty-d7b2df6a3a1f132e.d: src/main.rs
+
+src/main.rs:
