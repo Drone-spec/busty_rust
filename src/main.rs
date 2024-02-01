9619fc0b71b026da96361e1780bc23813081482b
drone-spec <drone@dronemothership.work>
2024-02-01 13:10:31 -0600

    I dont understand what I beaned

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,61 @@ fn add_task(task: Json<Task<'_>>) -> &'static str {
     "Task added succesfully"
 }
 
+#[get("/readtasks")]
+fn read_tasks() -> Json<Vec<String>> {
+    let tasks = OpenOptions::new()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open("tasks.txt")
+        .expect("unable to access tasks.txt");
+    let reader = BufReader::new(tasks);
+    Json(reader.lines()
+         .map(|line| {
+             let line_string: String = line.expect("could not read line");
+             let line_pieces: Vec<&str> = line_string.split(",").collect();
+             line_pieces[1].to_string()
+         })
+         .collect())
+}
+
+#[put("/edittask", data="<task_update>")]
+fn edit_task(task_update: Json<TaskUpdate<'_>>) -> &'static str(
+    let tasks = OpenOptions::new()
+    .read(true)
+    .append(true)
+    .create(true)
+    .open("tasks.txt")
+    .expect("unable to access temp.txt");
+
+    let mut temp = OpenOptions::new()
+    .create(true)
+    .append(true)
+    .create(true)
+    .open("temp.txt")
+    .expect("unable to access temp.txt");
+
+    let reader = BufReader::new(tasks);
+    for line in reader.lines() {
+        let line in reader.lines() {
+            let line_string: String = line.expect("Could not read line");
+            let line_pieces: Vec<&str> = line_string.split(",").collect();
+
+            if line_pieces[0].parse::<u8>().expect("unable to parse id as u8") == task_update.id {
+                let task_items: [&str; 2] = [line_pieces[0], task_update.item];
+                let task = format!("{}\n", task_items.join(","));
+                temp.write(task.as_bytes()).expect("Could not write to temp file");
+            }
+            else {
+                let task = format!("{}\n", task_items
+                temp.write(task.as_bytes()).expect("Could not write to the temp file");
+                }
+            }
+            std::fs::remove_file("tasks.txt").expect("unable to remove tasks.txt");
+            std::fs::rename("temp.txt", "tasks.txt").expect("unable to rename temp.txt");
+            "Task updated succesfully"
+            }
+
 
 #[get("/")]
 fn index() -> &'static str {
